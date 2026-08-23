075faf949352ae1f221b064e04a6daa9b63fc318
Jon Heinritz <jon.heinritz@protonmail.com>
2026-08-23 13:01:42 +0200

    added shell completions

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 use std::path::PathBuf;
 
 use clap::CommandFactory;
+use clap_complete::Shell;
+use clap_complete_nushell::Nushell;
 
 #[path = "src/bin/cli/mod.rs"]
 mod cli;
@@ -9,11 +11,34 @@ fn main() -> std::io::Result<()> {
     println!("cargo:rerun-if-changed=Cargo.toml");
     println!("cargo:rerun-if-changed=src/bin/cli/mod.rs");
 
-    let output =
-        PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?).join("man");
-    if output.exists() {
-        std::fs::remove_dir_all(&output)?;
+    let out_dir = PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
+
+    {
+        let output = out_dir.join("man");
+        if output.exists() {
+            std::fs::remove_dir_all(&output)?;
+        }
+        std::fs::create_dir_all(&output)?;
+        clap_mangen::generate_to(cli::Cli::command(), output)?;
     }
-    std::fs::create_dir_all(&output)?;
-    clap_mangen::generate_to(cli::Cli::command(), output)
+
+    {
+        let output = out_dir.join("completions");
+        if output.exists() {
+            std::fs::remove_dir_all(&output)?;
+        }
+        std::fs::create_dir_all(&output)?;
+        for shell in [
+            Shell::Bash,
+            Shell::Elvish,
+            Shell::Fish,
+            Shell::PowerShell,
+            Shell::Zsh,
+        ] {
+            clap_complete::generate_to(shell, &mut cli::Cli::command(), "vellum", &output)?;
+        }
+        clap_complete::generate_to(Nushell, &mut cli::Cli::command(), "vellum", &output)?;
+    }
+
+    Ok(())
 }
